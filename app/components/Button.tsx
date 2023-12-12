dd88054ef7eda3f4e264fe514e4430ec9c9b9bde
Thai Radin <Thai.R.360@gmail.com>
2023-12-11 21:35:43 -0500

    working

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,7 +1,18 @@
-const Button = () => {
-  return (
-    <button className="btn btn-neutral mt-5">Reveal</button>
-  )
-}
+"use client";
 
-export default Button
+import React, { useState } from "react";
+
+export default function Button() {
+  const [show, setShow] = useState(false);
+  return (
+    <div>
+      <button
+        type="button"
+        onClick={() => setShow(!show)}
+        className="btn btn-neutral mt-5">
+        Reveal
+      </button>
+      {show && <h2 className="mt-5">Grains...</h2>}
+    </div>
+  );
+}
